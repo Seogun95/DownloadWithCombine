9520240baf6893e1e79d66ad9915fc806f736f40
SeonJungKim <ksj083895@gmail.com>
2021-06-20 12:06:08 +0900

    Initial Commit

---
+++ DownloadWithCombine/DownloadWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadWithCombineApp.swift
+//  DownloadWithCombine
+//
+//  Created by Seogun Kim on 2021/06/20.
+//
+
+import SwiftUI
+
+@main
+struct DownloadWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
